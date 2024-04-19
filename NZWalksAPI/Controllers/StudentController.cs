afdbb73c0be592def03848d14c709108d9df94cd
VP <VP@JIBRIN-PC>
2024-04-19 23:45:51 +0100

    Add project files.

---
+++ NZWalksAPI/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentName = new string[] { "Johne", "Adam", "Musa", "Mark", "Emily" };
+
+            return Ok(studentName);
+        }
+    }
+}
